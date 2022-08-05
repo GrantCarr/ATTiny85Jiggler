7a7f80fda8bd09e0ee214c426413ffacdffe32a8
GrantCarr <36683467+GrantCarr@users.noreply.github.com>
2022-08-06 08:03:52 +1000

    Update MouseJiggler.ino

--- MouseJiggler.ino
+++ MouseJiggler.ino
@@ -1,18 +1,17 @@
 // Digispark Mouse Jiggler
-// Written by James Franklin for Air-Gap in 2019
-// www.air-gap.com.au
+
 
 #include <DigiMouse.h>
-unsigned int LowerCycleTime = 10000; //Minimum Time in milli-seconds between each mouse action  Default: 10000 (10 Seconds), Max 65535ms
-unsigned int UpperCycleTime = 30000; //Maximum Time in milli-seconds between each mouse action  Default: 30000 (30 Seconds), Max 65535ms
-//Random Function will randomly execute a mouse move between these two values
+unsigned int LowerCycleTime = 10000; //Minimum  65535ms
+unsigned int UpperCycleTime = 30000; //Maximum  65535ms
+
 void setup() {
   randomSeed(analogRead(0));  //Random Seed off background noise on analog pin
   pinMode(1, OUTPUT);
   DigiMouse.begin(); //start
- 
-  
 }
+
+
 void loop() {
 //Moves mouse 1 pixel in a direction (up/down/left/right) in a square
 
